e5e5944b4d9480c1d01c20d68afedbf83ecfff56
Jack <j.towler2@newcastle.ac.uk>
2022-10-02 13:30:41 +0100

    V2

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import random
 print ("Hello this is a dice simulator")
-goes = int(input("How many goes do you want to have?"))
+play=1
 counter = 0
 throw_count = 1
-dice = ["1", "2", "3", "4", "5", "6"]
-while goes != counter:
-    computer_action = random.choice(dice)
-    print (throw_count, ":  ", computer_action)
-    counter = counter + 1
-    throw_count = throw_count + 1
+while play == 1:
+    goes = int(input("How many goes do you want to have?"))
+    dice = ["1", "2", "3", "4", "5", "6"]
+    while goes != counter:
+        computer_action = random.choice(dice)
+        print (throw_count, ":  ", computer_action)
+        counter = counter + 1
+        throw_count = throw_count + 1
+    play = int(input("Would you like to play again? Press 1 for yes and 0 for no."))
+    if (play == 1):
+        counter = 0
+        throw_count = 1
+    else:
+        print ("Thank you for playing!")
